679f22f0de91fca0f5e794e544e672ef45f37ff2
StarkDroid <gamerprince.exp@gmail.com>
2021-05-01 17:55:43 +0530

    Init corvus
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- corvus_phoenix.mk
+++ corvus_phoenix.mk
@@ -6,20 +6,15 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common CorvusOS stuff.
+$(call inherit-product, vendor/corvus/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := corvus_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
-
-# ArrowOS Properties
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Sreekanth
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
 BUILD_DESCRIPTION := redfin-user 11 RQ2A.210405.005 7181113 release-keys
